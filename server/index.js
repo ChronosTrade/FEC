32a2b8e6f31a23bc631923ff9dace6d0de95c118
Jiahou Liu <reonaasaka@gmail.com>
2023-08-09 15:06:46 -0700

    Update main branch with lint config and fix syntax

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const express = require('express');
+require('dotenv').config();
 const path = require('path');
+const express = require('express');
 
 const app = express();
 
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
