aa3dc262884fd9dcb7b705d76e6f9539ec747bb0
Jiahou Liu <reonaasaka@gmail.com>
2023-08-11 11:10:06 -0700

    Update useContext and folder settings

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import ReviewMain from './reviews/ReviewMain';
+import AppContext from './AppContext';
 
 function App() {
-  return <div>Welcome to Our New Project!</div>;
+  const [totalRatings, setTotalRatings] = useState(0);
+
+  const contextValue = useMemo(
+    () => ({ totalRatings, setTotalRatings }),
+    [totalRatings, setTotalRatings],
+  );
+
+  return (
+    <AppContext.Provider value={contextValue}>
+      <ReviewMain />
+    </AppContext.Provider>
+  );
 }
 
 export default App;
